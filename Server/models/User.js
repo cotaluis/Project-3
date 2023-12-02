144a7e43d9faca9e3fa25b6087c78b9a4133eaf1
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-12-01 19:08:26 -0800

    added server folder

---
+++ Server/models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+      },
+}, {timestamps: true});
+
+const User = model('User', userSchema);
+
+module.exports = User;
